d5e6261fe51bee97d6e66795b3fe867e616e78a7
Petar Španja <petar@spanja.info>
2017-07-05 06:51:42 +0200

    Move overriden generator Word visitor

--- tests/lib/Search/Query/CriterionVisitor/FullTextTest.php
+++ tests/lib/Search/Query/CriterionVisitor/FullTextTest.php
@@ -14,7 +14,7 @@ use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use eZ\Publish\Core\FieldType\TextLine\SearchField;
 use eZ\Publish\Core\Search\Common\FieldNameResolver;
 use eZ\Publish\SPI\Search\FieldType\StringField;
-use EzSystems\EzPlatformSolrSearchEngine\Query\Common\QueryTranslator\WordVisitor;
+use EzSystems\EzPlatformSolrSearchEngine\Query\Common\QueryTranslator\Generator\WordVisitor;
 use EzSystems\EzPlatformSolrSearchEngine\Query\Content\CriterionVisitor\FullText;
 use EzSystems\EzPlatformSolrSearchEngine\Tests\Search\TestCase;
 use QueryTranslator\Languages\Galach\Generators;
